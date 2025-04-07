88af689573f834c9de3367b12cdc64dbd0ef4789
ramanrewati <ramanrewatiboss@gmail.com>
2025-04-07 17:15:43 +0530

    apix

--- api.py
+++ api.py
@@ -18,4 +18,4 @@ def recommend():
     return jsonify({"result": response})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
