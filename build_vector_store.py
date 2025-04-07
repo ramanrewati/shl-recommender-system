524b3824c5399394aedecbe236641cea16d35d28
ramanrewati <ramanrewatiboss@gmail.com>
2025-04-07 15:34:08 +0530

    deployment solved

--- build_vector_store.py
+++ build_vector_store.py
@@ -1,24 +1,30 @@
 from langchain.text_splitter import MarkdownHeaderTextSplitter
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceInferenceAPIEmbeddings
+
 
 def build_vector_store(md_file):
     with open(md_file, "r") as f:
         markdown = f.read()
-    
+
     headers_to_split_on = [
         ("#", "Header 1"),
         ("##", "Header 2"),
         ("###", "Header 3"),
     ]
-    
+
     splitter = MarkdownHeaderTextSplitter(
         headers_to_split_on=headers_to_split_on,
         return_each_line=False
     )
     chunks = splitter.split_text(markdown)
-    
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
+
+    embeddings = HuggingFaceInferenceAPIEmbeddings(
+        api_key=st.secrets["HF_TOKEN"],
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
     db = FAISS.from_documents(chunks, embeddings)
     db.save_local("faiss_index")
 
@@ -26,3 +32,6 @@ if __name__ == "__main__":
     doc_path = "/Users/rewatiramansingh/Desktop/Projects/shl-assignment/shl-assignment/data/shl-docs.md"
     build_vector_store(doc_path)
     print("Vector DB created!")
+
+# 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
+# AIzaSyCNuz86CuDzjIiesn5UDHz1wuxunQ_a04Q
